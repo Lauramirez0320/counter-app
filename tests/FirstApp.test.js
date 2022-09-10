9545ca6022f016056d476ec016856e50b317ea3e
LauRamirezG <laura.ramirez@mercadolibre.com.co>
2022-09-10 11:04:24 -0500

    First app test

---
+++ tests/FirstApp.test.js
@@ -0,0 +1,21 @@
+import {render} from '@testing-library/react';
+import { FirstApp } from '../src/FirstApp';
+
+describe('Testing FirstApp component', () => {
+  test('Should match snapshot', () => {
+    const { container } = render ( <FirstApp />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test('', () => {
+    const newMessageData = [{
+      message: "Hola Mundo",
+      title: "Laura",
+    }];
+
+    const {container, getByText} =  render(<FirstApp />);
+    expect( getByText())
+
+  })
+})
